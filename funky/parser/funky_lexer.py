be044ec8492df4a92bbe7abe5b7ff3b8ada8aaf5
Joe Ellis <joechrisellis@gmail.com>
2018-12-27 16:53:10 +0000

    Layout rule implemented

--- funky/parser/funky_lexer.py
+++ funky/parser/funky_lexer.py
@@ -1,82 +1,167 @@
+"""Lexical analyser for the funky programming language. Lexical analysis
+involves taking the source code as a raw string and splitting it down into
+'tokens' for easier parsing later.
+
+The lexical analysis phase for funky consists of two parts: the first involves
+taking the raw source and splitting it down into tokens; the second involves
+taking the result of the first stage and inserting any implicit braces. The way
+that we handle this is very similar to the 'layout' rule in Haskell.
+"""
+
 import ply.lex as lex
 
 from funky.util import err
 
 class FunkyLexer:
+    """PLY lexer for funky. This performs the first stage of lexical analysis
+    on the source code -- i.e. breaks down the source into "tokens" where a
+    token represents the basic unit of meaning.
+    """
 
+    # Reserved keywords -- variables may not have these names.
+    reserved = {
+        "module"  : "MODULE",
+        "import"  : "IMPORT",
+        "as"      : "AS",
+        "newtype" : "NEWTYPE",
+        "let"     : "LET",
+        "in"      : "IN",
+        "if"      : "IF",
+        "then"    : "THEN",
+        "else"    : "ELSE",
+        "where"   : "WHERE",
+        "match"   : "MATCH",
+        "of"      : "OF",
+        "infix"   : "INFIX",
+        "infixl"  : "INFIXL",
+        "infixr"  : "INFIXR",
+    }
+
+    # All the tokens known to the lexer.
     tokens = [
-        # variables and literals
-        "IDENTIFIER",
-        "NUMBER",
+
+        # structural
+        "COMMENT",
+        "INDENTATION",
+
+        # 'control' characters
+        "BACKTICK",
+        "COMMA",
+        "TYPESIG",
+        "CONSTRUCTOR",
+        "LAMBDA",
+        "PIPE",
+        "WILDCARD",
+        "ARROW",
+        "ENDSTATEMENT",
+
+        # literals
         "FLOAT",
+        "INTEGER",
         "CHAR",
         "STRING",
 
         # math-related operators
+        "EQUALITY",
+        "INEQUALITY",
+        "LESS",
+        "LEQ",
+        "GREATER",
+        "GEQ",
         "EQUALS",
+        "POW",
         "PLUS",
         "MINUS",
         "TIMES",
         "DIVIDE",
-        "POW",
-
-        # miscellaneous operators
-        "ARROW",
-        "GUARD",
 
         # brackets
-        "OPEN_BRACKET",
-        "CLOSE_BRACKET",
+        "OPEN_PAREN",
+        "CLOSE_PAREN",
+        "OPEN_SQUARE",
+        "CLOSE_SQUARE",
+        "OPEN_BRACE",
+        "CLOSE_BRACE",
 
-        # keywords
-        "LET",
-        "IN",
-        "IF",
-        "ELSE",
-        "WHERE",
-        "MATCH",
+        # identifiers and labels
+        "IDENTIFIER",
+        "TYPENAME",
+    ] + list(reserved.values())
 
-        # miscellaneous
-        "NEWLINE",
-    ]
+    # Ignore spaces and tabs (unless they appear in the context of another
+    # lexeme)
+    t_ignore        =  " \t"
 
-    t_ignore        = r" " # ignore whitespace.
-    t_IDENTIFIER    = r"[A-Za-z][A-za-z0-9_]*"
+    # Comments are the same as they are in python -- this function returns
+    # nothing since comments are ignored.
+    def t_COMMENT(self, t):
+        r"\n*[ ]*\#[^\n]*"
+        pass
 
-    def t_NUMBER(self, t):
-        r"\d+"
-        t.value = int(t.value)
+    # Indentation is considered to be one or more line breaks followed by some
+    # spacing.
+    def t_INDENTATION(self, t):
+        r"\n+[ ]*"
+        t.value = t.value.count(" ") # level of indentation = number of spaces
         return t
 
+    # Regexes for the control characters.
+    t_BACKTICK      =  r"`"
+    t_COMMA         =  r","
+    t_TYPESIG       =  r"::"
+    t_CONSTRUCTOR   =  r":"
+    t_LAMBDA        =  r"\\"
+    t_PIPE          =  r"\|"
+    t_WILDCARD      =  r"_"
+    t_ARROW         =  r"->"
+    t_ENDSTATEMENT  =  r";"
+
+    # Literals:
+    # Floating point numbers.
     def t_FLOAT(self, t):
         r"\d+\.\d+"
         t.value = float(t.value)
         return t
 
-    t_CHAR          = r"'[.]'"
-    t_STRING        = r"(\".*?\"|'.*?')"
+    # Integers.
+    def t_INTEGER(self, t):
+        r"\d+"
+        t.value = int(t.value)
+        return t
 
-    t_EQUALS        = r"="
-    t_PLUS          = r"\+"
-    t_MINUS         = r"-"
-    t_TIMES         = r"\*"
-    t_DIVIDE        = r"/"
-    t_POW           = r"\*\*"
+    t_CHAR          =  r"'[.]'"
+    t_STRING        =  r"(\".*?\"|'.*?')"
 
-    t_ARROW         = r"->"
-    t_GUARD         = r"\|"
+    # Math operators.
+    t_EQUALITY      =  r"=="
+    t_INEQUALITY    =  r"!="
+    t_LESS          =  r"<"
+    t_LEQ           =  r"<="
+    t_GREATER       =  r">"
+    t_GEQ           =  r">="
+    t_EQUALS        =  r"="
+    t_POW           =  r"\*\*"
+    t_PLUS          =  r"\+"
+    t_MINUS         =  r"-"
+    t_TIMES         =  r"\*"
+    t_DIVIDE        =  r"/"
 
-    t_OPEN_BRACKET  = r"\("
-    t_CLOSE_BRACKET = r"\)"
+    # Brackets.
+    t_OPEN_PAREN    =  r"\("
+    t_CLOSE_PAREN   =  r"\)"
+    t_OPEN_SQUARE   =  r"\["
+    t_CLOSE_SQUARE  =  r"\]"
+    t_OPEN_BRACE    =  r"\{"
+    t_CLOSE_BRACE   =  r"\}"
 
-    t_LET           = r"let"
-    t_IN            = r"in"
-    t_IF            = r"if"
-    t_ELSE          = r"else"
-    t_WHERE         = r"where"
-    t_MATCH         = r"match"
+    # An identifier -- checks for keywords.
+    def t_IDENTIFIER(self, t):
+         r"[a-z][A-Za-z0-9_]*"
+         t.type = self.reserved.get(t.value, "IDENTIFIER") # Check for keywords
+         return t
 
-    t_NEWLINE       = r"\n+"
+    # Typenames are always capitalised, as in Haskell.
+    t_TYPENAME      = r"[A-Z][A-Za-z]*"
 
     def t_error(self, t):
         err("Cannot lex input.") # TODO -- make an exception here.
@@ -85,11 +170,100 @@ class FunkyLexer:
         """Build the lexer."""
         self.lexer = lex.lex(module=self, **kwargs)
     
-    def do_lex(self, data):
-        self.lexer.input(data)
+    def do_lex(self, source, *args, **kwargs):
+        self.lexer.input(source, *args, **kwargs)
+
         tokens = []
         while True:
             tok = self.lexer.token()
             if not tok: break
             tokens.append(tok)
+
         return tokens
+
+class IndentationLexer:
+    """Indentation lexer. This performs the second stage of lexical analysis --
+    here, we find where braces and semicolons have been omitted (implicit
+    braces inferred from indentation) and insert them as literal characters
+    into the token stream.
+    """
+
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.ind_stack = []
+        self.new_tokens = []
+        self.source = ""
+
+    def input(self, source, *args, **kwargs):
+        self.source = source
+        self.orig_tokens = self.lexer.do_lex(source, *args, **kwargs)
+        self.new_tokens = self._insert_implicit_tokens(self.orig_tokens)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        tok = self.token()
+        if not tok:
+            raise StopIteration()
+        return tok
+
+    def token(self):
+        if self.new_tokens:
+            return self.new_tokens.pop(0)
+
+    def _insert_implicit_tokens(self, tokens):
+        """Inserts explicit braces into a token stream containing omitted
+        braces.
+        """
+        new_tokens = []
+
+        ind_stack = []
+        
+        i = 0
+        while i < len(tokens) - 1:
+            tok, next_tok = tokens[i], tokens[i + 1]
+            if tok.type in ["WHERE", "LET", "OF"] and \
+               next_tok.type != "OPEN_BRACE":
+                j = i + 1
+                while next_tok.type == "INDENTATION":
+                    j += 1
+                    next_tok = tokens[j]
+                ind_stack.append(self._find_column(next_tok))
+                new_tokens.append(tok)
+                new_tokens.append(self._make_token("OPEN_BRACE", value="{"))
+            elif tok.type == "INDENTATION":
+                if ind_stack:
+                    if tok.value == ind_stack[-1]:
+                        new_tokens.append(self._make_token("ENDSTATEMENT", value=";"))
+                    elif tok.value < ind_stack[-1]:
+                        new_tokens.append(self._make_token("CLOSE_BRACE", value="}"))
+                        ind_stack.pop()
+                        continue
+            else:
+                new_tokens.append(tok)
+
+            i += 1
+
+        while ind_stack:
+            new_tokens.append(self._make_token("CLOSE_BRACE", value="}"))
+            ind_stack.pop()
+
+        return new_tokens
+
+    def _make_token(self, type, value=None, lineno=None, lexpos=None):
+        tok = lex.LexToken()
+        tok.type = type
+        tok.value = value
+        tok.lineno = 0
+        tok.lexpos = 0
+        return tok
+
+    def _find_column(self, token):
+        """Given the source string and a token from it (found by the lexer), gets
+        the column in the line that the token appeared in. This is required for
+        layout ruling.
+        """
+        line_start = self.source.rfind("\n", 0, token.lexpos) + 1
+        retval = (token.lexpos - line_start)
+        return retval
--- funky/parser/funky_parser.py
+++ funky/parser/funky_parser.py
@@ -1,93 +1,298 @@
 import ply.yacc as yacc
 
 from funky.util import err
-from funky.parser.funky_lexer import FunkyLexer
-
-def err(*args):
-    print("AHHHHH")
+from funky.parser.funky_lexer import FunkyLexer, IndentationLexer
 
 class FunkyParser:
 
     tokens = FunkyLexer.tokens
-    start = "PROG"
+    start = "MODULE_DEFINITION"
     precedence = (
-        ("left", "POW"),
+        ("nonassoc", "EQUALITY"),
+        ("left", "LESS", "LEQ", "GREATER", "GEQ"),
         ("left", "PLUS", "MINUS"),
         ("left", "TIMES", "DIVIDE"),
-        ("right", "UMINUS"),
+        ("right", "POW"),
     )
 
-    def p_PROG(self, p):
-        """PROG : FUNC_LIST"""
-        pass
-
-    def p_FUNC_LIST(self, p):
-        """FUNC_LIST : FUNC NEWLINE FUNC_LIST
-                     | empty
+    def p_MODULE_DEFINITION(self, p):
+        """MODULE_DEFINITION : MODULE IDENTIFIER WHERE BODY
+                             | BODY
         """
         pass
 
-    def p_FUNC(self, p):
-        """FUNC : IDENTIFIER ARG_LIST EQUALS EXPRESSION"""
-        pass
-
-    def p_ARG_LIST(self, p):
-        """ARG_LIST : IDENTIFIER ARG_LIST
-                    | NUMBER ARG_LIST
-                    | STRING ARG_LIST
-                    | IDENTIFIER
-                    | NUMBER
-                    | STRING
+    def p_BODY(self, p):
+        """BODY : OPEN_BRACE IMPORT_DECLARATIONS ENDSTATEMENT TOP_DECLARATIONS CLOSE_BRACE
+                | OPEN_BRACE IMPORT_DECLARATIONS CLOSE_BRACE
+                | OPEN_BRACE TOP_DECLARATIONS CLOSE_BRACE
         """
         pass
 
-    def p_EXPRESSION_GROUP(self, p):
-        """EXPRESSION : OPEN_BRACKET EXPRESSION CLOSE_BRACKET"""
-        pass
-
-    def p_EXPRESSION_BINOP(self, p):
-        """EXPRESSION : EXPRESSION PLUS EXPRESSION
-                      | EXPRESSION MINUS EXPRESSION
-                      | EXPRESSION TIMES EXPRESSION
-                      | EXPRESSION DIVIDE EXPRESSION
-                      | EXPRESSION POW EXPRESSION
+    def p_IMPORT_DECLARATIONS(self, p):
+        """IMPORT_DECLARATIONS : IMPORT_DECLARATIONS ENDSTATEMENT IMPORT_DECLARATION
+                               | IMPORT_DECLARATION
         """
         pass
 
-    def p_EXPRESSION_UMINUS(self, p):
-        """EXPRESSION : MINUS EXPRESSION %prec UMINUS"""
+    def p_IMPORT_DECLARATION(self, p):
+        """IMPORT_DECLARATION : IMPORT IDENTIFIER ENDSTATEMENT
+                              | IMPORT IDENTIFIER AS IDENTIFIER ENDSTATEMENT
+        """
+        # TODO: may have to change IDENTIFIER to a more robust module ID here.
         pass
 
-    def p_EXPRESSION_FUNCTION_APP(self, p):
-        """EXPRESSION : IDENTIFIER ARG_LIST"""
+    def p_TOP_DECLARATIONS(self, p):
+        """TOP_DECLARATIONS : TOP_DECLARATIONS ENDSTATEMENT TOP_DECLARATION
+                            | TOP_DECLARATION
+        """ # TODO: you cannot have an empty list here, if it matters.
         pass
 
-    def p_EXPRESSION_LET(self, p):
-        """EXPRESSION : LET IDENTIFIER EQUALS EXPRESSION IN EXPRESSION"""
+    def p_TOP_DECLARATION(self, p):
+        """TOP_DECLARATION : NEWTYPE TYPENAME EQUALS TYPENAME ENDSTATEMENT
+                           | DECLARATION
+        """
         pass
 
-    def p_EXPRESSION_IF(self, p):
-        """EXPRESSION : EXPRESSION IF EXPRESSION ELSE EXPRESSION"""
+    def p_DECLARATIONS(self, p):
+        """DECLARATIONS : OPEN_BRACE DECLARATIONS_LIST CLOSE_BRACE
+                        | OPEN_BRACE CLOSE_BRACE
+        """
         pass
 
-    def p_EXPRESSION_WHERE(self, p):
-        """EXPRESSION : EXPRESSION WHERE IDENTIFIER EQUALS EXPRESSION"""
+    def p_DECLARATIONS_LIST(self, p):
+        """DECLARATIONS_LIST : DECLARATION ENDSTATEMENT DECLARATIONS_LIST
+                             | DECLARATION
+        """
         pass
 
-    def p_EXPRESSION_NUMBER(self, p):
-        """EXPRESSION : NUMBER"""
+    def p_DECLARATION(self, p):
+        """DECLARATION : GEN_DECLARATION
+                       | FUNCTION_LHS RHS
+                       | PAT RHS
+        """
+        pass
+
+    def p_GEN_DECLARATION(self, p):
+        """GEN_DECLARATION : VARS TYPESIG TYPE
+                           | FIXITY INTEGER OPS
+                           | FIXITY OPS
+                           |
+        """
+        pass
+
+    def p_OPS(self, p):
+        """OPS : OPS COMMA OP
+               | OP
+        """
+        pass
+
+    def p_VARS(self, p):
+        """VARS : VARS COMMA IDENTIFIER
+                | IDENTIFIER
+        """
+        pass
+
+    def p_FIXITY(self, p):
+        """FIXITY : INFIXL
+                  | INFIXR
+                  | INFIX
+        """
+        pass
+
+    def p_TYPE(self, p):
+        """TYPE : BTYPE
+                | BTYPE ARROW TYPE
+        """
+        pass
+
+    def p_BTYPE(self, p):
+        """BTYPE : ATYPE
+                 | BTYPE ATYPE
+        """
+        pass
+
+    def p_ATYPE(self, p):
+        """ATYPE : TYPENAME
+                 | OPEN_PAREN TYPES_LIST CLOSE_PAREN
+                 | OPEN_PAREN TYPE CLOSE_PAREN
+                 | OPEN_SQUARE TYPE CLOSE_SQUARE
+        """
+        pass
+
+    def p_FUNCTION_LHS(self, p):
+        """FUNCTION_LHS : IDENTIFIER APAT APATS
+                        | PAT VAROP PAT
+                        | OPEN_PAREN FUNCTION_LHS CLOSE_PAREN APAT APATS
+        """
+        pass
+
+    def p_RHS(self, p):
+        """RHS : EQUALS EXP
+               | EQUALS EXP WHERE DECLARATIONS
+               | GDRHS
+               | GDRHS WHERE DECLARATIONS
+        """
+        pass
+
+    def p_GDRHS(self, p):
+        """GDRHS : GUARDS EQUALS EXP
+                 | GUARDS EQUALS EXP GDRHS
+        """
+        pass
+
+    def p_GUARDS(self, p):
+        """GUARDS : PIPE GUARD_LIST
+        """
+        pass
+
+    def p_GUARD_LIST(self, p):
+        """GUARD_LIST : GUARD_LIST GUARD
+                      | GUARD
+        """
+        pass
+
+    def p_GUARD(self, p):
+        """GUARD : INFIX_EXP
+        """ # we only allow for BOOLEAN guards.
+        pass
+
+    def p_EXP(self, p):
+        """EXP : INFIX_EXP
+        """
         pass
     
-    def p_EXPRESSION_VARIABLE(self, p):
-        """EXPRESSION : IDENTIFIER"""
+    def p_INFIX_EXP(self, p):
+        """INFIX_EXP : LEXP OP INFIX_EXP
+                     | MINUS INFIX_EXP
+                     | LEXP
+        """ # did use qualified operators before.
         pass
 
-    def p_EXPRESSION_STRING(self, p):
-        """EXPRESSION : STRING"""
+    def p_LEXP(self, p):
+        """LEXP : LAMBDA APAT APATS ARROW EXP
+                | LET DECLARATIONS IN EXP
+                | IF EXP THEN EXP ELSE EXP
+                | MATCH EXP OF OPEN_BRACE ALTS CLOSE_BRACE
+                | FEXP
+        """
         pass
-     
-    def p_empty(self, p):
-        """empty :"""
+
+    def p_FEXP(self, p):
+        """FEXP : FEXP AEXP
+                | AEXP
+        """
+        pass
+
+    def p_AEXP(self, p):
+        """AEXP : IDENTIFIER
+                | GCON
+                | LITERAL
+                | OPEN_PAREN EXP CLOSE_PAREN
+                | OPEN_PAREN EXP COMMA EXP_LIST CLOSE_PAREN
+                | OPEN_SQUARE EXP CLOSE_SQUARE
+                | OPEN_SQUARE EXP COMMA EXP_LIST CLOSE_SQUARE
+        """ # few things missing, should be ok tho
+        pass
+
+    def p_ALTS(self, p):
+        """ALTS : ALTS ALT ENDSTATEMENT
+                | ALT
+        """
+        pass
+
+    def p_ALT(self, p):
+        """ALT : PAT ARROW EXP
+               |
+        """
+        pass
+
+    def p_PAT(self, p):
+        """PAT : LPAT CONSTRUCTOR PAT
+               | LPAT
+        """
+        pass
+
+    def p_LPAT(self, p):
+        """LPAT : APAT
+                | MINUS OPEN_PAREN INTEGER CLOSE_PAREN
+                | MINUS OPEN_PAREN FLOAT CLOSE_PAREN
+        """ # TODO: gcon apat1 … apatk
+        pass
+
+    def p_APAT(self, p):
+        """APAT : IDENTIFIER
+                | GCON
+                | LITERAL
+                | WILDCARD
+                | OPEN_PAREN PAT CLOSE_PAREN
+                | OPEN_PAREN PAT COMMA PAT_LIST CLOSE_PAREN
+                | OPEN_SQUARE PAT_LIST CLOSE_SQUARE
+        """
+        # TODO: might need to add qcon {fpat, ...} here
+        # also irrefutable pattern? Maybe.
+        pass
+
+    def p_GCON(self, p):
+        """GCON : OPEN_PAREN CLOSE_PAREN
+                | OPEN_SQUARE CLOSE_SQUARE
+        """ # TODO: might need to add QCON here
+        pass
+
+    def p_VAROP(self, p):
+        """VAROP : VARSYM
+                 | BACKTICK IDENTIFIER BACKTICK
+        """
+        pass
+
+    def p_OP(self, p):
+        """OP : VAROP
+        """
+        pass
+
+    def p_EXP_LIST(self, p):
+        """EXP_LIST : EXP_LIST COMMA EXP
+                    | EXP
+        """
+        pass
+
+    def p_APATS(self, p):
+        """APATS : APAT APATS
+                 |
+        """
+        pass
+
+    def p_PAT_LIST(self, p):
+        """PAT_LIST : PAT_LIST COMMA PAT
+                    | PAT
+        """
+        pass
+
+    def p_VARSYM(self, p):
+        """VARSYM : PLUS
+                  | MINUS
+                  | TIMES
+                  | DIVIDE
+                  | POW
+                  | EQUALITY
+                  | LESS
+                  | LEQ
+                  | GREATER
+                  | GEQ
+        """
+        pass
+
+    def p_TYPES_LIST(self, p):
+        """TYPES_LIST : TYPES_LIST COMMA TYPE
+                      | TYPE
+        """
+        pass
+
+    def p_LITERAL(self, p):
+        """LITERAL : INTEGER
+                   | FLOAT
+                   | CHAR
+                   | STRING
+        """
         pass
 
     def p_error(self, p):
@@ -96,7 +301,14 @@ class FunkyParser:
     def build(self, **kwargs):
         self.lexer = FunkyLexer()
         self.lexer.build()
+        self.lexer = IndentationLexer(self.lexer)
         self.parser = yacc.yacc(module=self, **kwargs)
 
     def do_parse(self, source):
-        return self.parser.parse(source)
+        self.lexer.input(source)
+        # TODO -- remove later.
+        for tok in self.lexer:
+            print(tok.value, end=" ")
+            if tok.value == ";":
+                print()
+        return self.parser.parse(source, self.lexer)

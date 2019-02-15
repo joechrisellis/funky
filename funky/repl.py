c177bd2254c466f04eca548fc6cd092bc5e409f9
Joe Ellis <joechrisellis@gmail.com>
2019-02-15 16:14:24 +0000

    More REPL work

--- funky/repl.py
+++ funky/repl.py
@@ -17,13 +17,68 @@ from funky.desugar.desugar import do_desugar, condense_function_binds
 from funky.infer.infer import do_type_inference, infer
 from funky.generate.gen_python import PythonCodeGenerator
 
-class FunkyShell(cmd.Cmd):
+class CustomCmd(cmd.Cmd):
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.identchars += ":"
+
+    def onecmd(self, line):
+        """Interpret the argument as though it had been typed in response
+        to the prompt.
+        This may be overridden, but should not normally need to be;
+        see the precmd() and postcmd() methods for useful execution hooks.
+        The return value is a flag indicating whether interpretation of
+        commands by the interpreter should stop.
+        """
+        cmd, arg, line = self.parseline(line)
+        if not line:
+            return self.emptyline()
+        if cmd is None:
+            return self.default(line)
+        self.lastcmd = line
+        if line == 'EOF' :
+            self.lastcmd = ''
+        if cmd == '':
+            return self.default(line)
+        else:
+            try:
+                cmd = cmd[1:] # drop the ':'
+                func = getattr(self, 'do_' + cmd)
+            except AttributeError:
+                return self.default(line)
+            return func(arg)
+
+    def parseline(self, line):
+        """Parse the line into a command name and a string containing
+        the arguments.  Returns a tuple containing (command, args, line).
+        'command' and 'args' may be None if the line couldn't be parsed.
+        """
+        line = line.strip()
+        if not line:
+            return None, None, line
+        elif line[0] == '?':
+            line = 'help ' + line[1:]
+        elif line[0] == '!':
+            if hasattr(self, 'do_shell'):
+                line = 'shell ' + line[1:]
+            else:
+                return None, None, line
+        elif line[0] == ":":
+            i, n = 0, len(line)
+            while i < n and line[i] in self.identchars: i = i+1
+            cmd, arg = line[:i], line[i:].strip()
+            return cmd, arg, line
+        else:
+            return None, None, line
+
+class FunkyShell(CustomCmd):
 
     intro   =  "funky ({}) shell".format(__version__)
     prompt  =  "funky> "
 
     def __init__(self):
-        cmd.Cmd.__init__(self)
+        super().__init__()
         self.decl_parser = FunkyParser()
         self.decl_parser.build(start="TOP_DECLARATIONS")
         self.expr_parser = FunkyParser()
@@ -36,10 +91,11 @@ class FunkyShell(cmd.Cmd):
     def do_begin_block(self, arg):
         """Start a block of definitions."""
         block_prompt = "block> "
+        end_block = ":end_block"
         lines = []
         while True:
             inp = input(block_prompt)
-            if inp == "end_block":
+            if inp == end_block:
                 break
             lines.append(" " + inp)
         self.run_lines(lines)
@@ -50,7 +106,7 @@ class FunkyShell(cmd.Cmd):
         self.global_let.expr = expr
         try:
             do_type_inference(self.global_let, typedefs)
-            print(self.global_let.inferred_type)
+            print("{} :: {}".format(arg, self.global_let.inferred_type))
         except FunkyTypeError:
             print("Expression is not type correct.")
 
@@ -61,10 +117,6 @@ class FunkyShell(cmd.Cmd):
     def do_let(self, arg):
         """Bind a name on a single line."""
         self.run_lines([arg])
-
-    def do_exec(self, arg):
-        code = self.get_compiled(arg)
-        exec(code, {"__name__" : "__main__"})
 
     def do_show(self, arg):
         code = self.get_compiled(arg)
@@ -111,9 +163,17 @@ class FunkyShell(cmd.Cmd):
         print("EOF, exiting.")
         exit(0)
 
+    def default(self, arg):
+        code = self.get_compiled(arg)
+        exec(code, {"__name__" : "__main__"})
+
+    def emptyline(self):
+        pass
+
 def main():
     try:
-        FunkyShell().cmdloop()
+        shell = FunkyShell()
+        shell.cmdloop()
     except KeyboardInterrupt:
         print("\nInterrupt caught, exiting.")
         exit(0)

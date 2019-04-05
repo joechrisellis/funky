111416a1138e5309e8c3b8cbe1f3dc432d7076c1
Joe Ellis <joechrisellis@gmail.com>
2019-04-05 21:35:01 +0100

    --dump-types fully fledged!

--- funky/compiler.py
+++ funky/compiler.py
@@ -134,7 +134,12 @@ def compiler_inference(core_tree, typedefs, dump_types):
     """
     do_type_inference(core_tree, typedefs)
     if dump_types:
-        print(cblue("## CORE TYPES"))
+        print(cblue("## ALGEBRAIC DATA TYPES"))
+        for typedef in typedefs:
+            print(typedef)
+
+        print("")
+        print(cblue("## TYPES"))
         for bind in core_tree.binds:
             print("{} :: {}".format(bind.identifier, bind.bindee.inferred_type))
         print("")
